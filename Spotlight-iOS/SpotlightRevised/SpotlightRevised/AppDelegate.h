d88e737190758acfe435eddaa9660a555aafd350
Kunal Shah <skunal8197@gmail.com>
2016-03-08 13:36:24 -0600

    created new project and major dependencies

---
+++ Spotlight-iOS/SpotlightRevised/SpotlightRevised/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotlightRevised
+//
+//  Created by Kunal Shah on 3/8/16.
+//  Copyright © 2016 Spotlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spotlight-iOS/SpotlightRevised/SpotlightRevised/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotlightRevised
+//
+//  Created by Kunal Shah on 3/8/16.
+//  Copyright © 2016 Spotlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
