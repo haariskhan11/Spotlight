6fc630a79a82035370540a715f9de78259516cab
Kunal Shah <skunal8197@gmail.com>
2016-04-27 14:38:40 -0500

    added swipeable cell to allow comments

--- Spotlight-iOS/SpotlightRevised/SpotlightRevised/TrendingViewCustom.m
+++ Spotlight-iOS/SpotlightRevised/SpotlightRevised/TrendingViewCustom.m
@@ -26,7 +26,8 @@
 @end
 
 @implementation TrendingViewCustom
-    
+
+
 
 
 -(BOOL)prefersStatusBarHidden{return YES;}
@@ -155,7 +156,14 @@
 //            [shareActionSheet showInView:self.view];
 //            
 //            [cell hideUtilityButtonsAnimated:YES];
-            [self performSegueWithIdentifier:@"trumpcomment" sender:self];
+            
+            //[self performSegueWithIdentifier:@"trumpcomment" sender:self];
+            
+            
+//            [self showPopupFormSheet:self];
+//            [self showPopupFullscreen:self];
+            [self showPopupCentered:self];
+            
             break;
         }
         case 1:
@@ -236,9 +244,90 @@
     [self presentViewController:playerViewController animated:YES completion:nil];
 }
 
--(void)vc{
+# pragma  mark - swipe cell 
+
+- (void)showPopupWithStyle:(CNPPopupStyle)popupStyle {
     
+    NSMutableParagraphStyle *paragraphStyle = NSMutableParagraphStyle.new;
+    paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    
+    NSAttributedString *title = [[NSAttributedString alloc] initWithString:@"Comment View" attributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:24], NSParagraphStyleAttributeName : paragraphStyle}];
+//    NSAttributedString *lineOne = [[NSAttributedString alloc] initWithString:@"You can add text and images" attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:18], NSParagraphStyleAttributeName : paragraphStyle}];
+//    NSAttributedString *lineTwo = [[NSAttributedString alloc] initWithString:@"With style, using NSAttributedString" attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:18], NSForegroundColorAttributeName : [UIColor colorWithRed:0.46 green:0.8 blue:1.0 alpha:1.0], NSParagraphStyleAttributeName : paragraphStyle}];
+    
+    CNPPopupButton *button = [[CNPPopupButton alloc] initWithFrame:CGRectMake(0, 0, 100, 30)];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [button setTitle:@"Post" forState:UIControlStateNormal];
+    button.backgroundColor = [UIColor colorWithRed:151.0/255.0 green:216.0/255.0 blue:192.0/255.0 alpha:100.0];
+    button.layer.cornerRadius = 4;
+    button.selectionHandler = ^(CNPPopupButton *button){
+        [self.popupController dismissPopupControllerAnimated:YES];
+        NSLog(@"Block for button: %@", button.titleLabel.text);
+    };
+    
+    UIImageView *image = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
+    image.image = [UIImage imageNamed:@"comments.png"];
+
+    
+    UITextField *comment = [[UITextField alloc] initWithFrame:CGRectMake(10, 10, 270, 40)];
+    comment.borderStyle = UITextBorderStyleRoundedRect;
+    comment.placeholder = @"Type here";
+    
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.numberOfLines = 0;
+    titleLabel.attributedText = title;
+    
+//    UILabel *lineOneLabel = [[UILabel alloc] init];
+//    lineOneLabel.numberOfLines = 0;
+//    lineOneLabel.attributedText = lineOne;
+    
+//    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon"]];
+    
+//    UILabel *lineTwoLabel = [[UILabel alloc] init];
+//    lineTwoLabel.numberOfLines = 0;
+//    lineTwoLabel.attributedText = lineTwo;
+    
+    
+    
+    UITextField *textFied = [[UITextField alloc] initWithFrame:CGRectMake(10, 10, 350, 35)];
+    textFied.borderStyle = UITextBorderStyleRoundedRect;
+    textFied.placeholder = @"Custom view!";
+//    [customView addSubview:textFied];
+    self.popupController = [[CNPPopupController alloc]initWithContents:@[titleLabel, comment, button]];
+    
+//    self.popupController = [[CNPPopupController alloc] initWithContents:@[titleLabel, lineOneLabel, imageView, lineTwoLabel, customView, button]];
+    self.popupController.theme = [CNPPopupTheme defaultTheme];
+    self.popupController.theme.popupStyle = popupStyle;
+    self.popupController.delegate = self;
+    [self.popupController presentPopupControllerAnimated:YES];
 }
+
+#pragma mark - CNPPopupController Delegate
+
+- (void)popupController:(CNPPopupController *)controller didDismissWithButtonTitle:(NSString *)title {
+    NSLog(@"Dismissed with button title: %@", title);
+}
+
+- (void)popupControllerDidPresent:(CNPPopupController *)controller {
+    NSLog(@"Popup controller presented.");
+}
+
+#pragma mark - event response
+
+-(void)showPopupCentered:(id)sender {
+    [self showPopupWithStyle:CNPPopupStyleCentered];
+}
+
+- (void)showPopupFormSheet:(id)sender {
+    [self showPopupWithStyle:CNPPopupStyleActionSheet];
+}
+
+- (void)showPopupFullscreen:(id)sender {
+    [self showPopupWithStyle:CNPPopupStyleFullscreen];
+}
+
 
 
 @end
